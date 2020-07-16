930563444c50d78856a8b545c6dd3eebf7e6bd3f
uedayusuke <uedayusuke1107@gmail.com>
2020-07-16 11:09:05 +0000

    make_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
