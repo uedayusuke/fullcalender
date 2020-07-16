9951ae79ba263066fd1875743863cfc83abf7d86
uedayusuke <uedayusuke1107@gmail.com>
2020-07-16 11:15:14 +0000

    edit_migrate

--- db/migrate/20200716111009_create_events.rb
+++ db/migrate/20200716111009_create_events.rb
@@ -1,7 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.string :title
+      t.datetime :start
+      t.datetime :end
       t.timestamps
     end
   end
